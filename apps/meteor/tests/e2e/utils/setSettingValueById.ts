064be1a522710f99997e02018bdd0ab98d717d33
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-11-27 18:16:20 -0300

    test: improve status check for useSettingValueById

--- apps/meteor/tests/e2e/utils/setSettingValueById.ts
+++ apps/meteor/tests/e2e/utils/setSettingValueById.ts
@@ -5,7 +5,7 @@ import type { BaseTest } from './test';
 export const setSettingValueById = async (api: BaseTest['api'], settingId: string, value: unknown, strict = true): Promise<APIResponse> => {
 	const response = await api.post(`/settings/${settingId}`, { value });
 
-	if (strict && response.status() !== 200) {
+	if (strict && !response.ok()) {
 		throw new Error(
 			`Failed to update setting "${settingId}" with value "${value}" [http status: ${response.status()} ${response.statusText()}]`,
 		);
